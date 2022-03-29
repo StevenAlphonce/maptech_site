68d3d75e0b0dd4de371f503038f499c645f68d85
Alphonce Stephano <alphoncesteven@gmail.com>
2022-03-29 19:30:17 +0300

    Top bar and menu bar -first draft

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="layout/bootstrap.css">
+
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Map Technologies | Home</title>
+
+    <!--HEADER TOP-->
+<header class="container-fluid bg-dark py-2">
+    <div class="float-left text-light">Welcome Map technologies repair services </div>
+    <div class="float-right">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#"><span class="fa fa-twitter"></span></a>
+        <a href="#"><span class="fa fa-google-plus"></span></a>
+        <a href="#"><span class="fa fa-pinterest-p"></span></a>
+    </div><br>
+</header>
+<!--/HEADER TOP-->
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once 'includes/header.php'?>
+    
+<?php require_once 'includes/menu_bar.php'?>
+
+<div class="container">
+
+</div>
+ 
+    <?php require_once 'includes/footer.php'?>   
